6bd8ed5edb3bb6adc7d16f2a2367ce143d180cb9
rahaf89 <rahaflh.900@hotmail.com>
2020-01-18 07:37:45 -0500

    movietitle

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const API_KEY = "fcabf0c452904d4f09e359d56ce644ce";
-
+const BASE_URL = "https://api.themoviedb.org/3";
 let searchbox = document.getElementById("searchbox");
 
 searchbox.addEventListener("keyup", onSearch);
@@ -10,4 +10,19 @@ function onSearch(event) {
   }
 }
 
-function fetchMovies(query) {}
+function fetchMovies(query) {
+  const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=movie&page=${query}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(responseBody => showMovies(responseBody.results));
+}
+
+function showMovies(movies) {
+  for (let i = 0; i < movies.length; i++) {
+    showMovie(movies[i]);
+  }
+}
+
+function showMovie(movie) {
+  console.log(movie.title);
+}
