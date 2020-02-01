d4822ca2efacbb5bf746d8b4fd9210b6399ef102
rahaf89 <rahaflh.900@hotmail.com>
2020-02-01 15:44:10 -0500

    Finder

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 const API_KEY = "fcabf0c452904d4f09e359d56ce644ce";
 const BASE_URL = "https://api.themoviedb.org/3";
-let searchbox = document.getElementById("searchbox");
-
-searchbox.addEventListener("keyup", onSearch);
-
+let searchBox = document.getElementById("searchbox");
+searchBox.addEventListener("keyup", onSearch);
 function onSearch(event) {
   if (event.keyCode == 13) {
-    fetchMovies(searchbox.value);
+    fetchMovies(searchBox.value);
   }
 }
-
 function fetchMovies(query) {
-  const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=movie&page=${query}`;
+  const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`;
   fetch(url)
     .then(response => response.json())
     .then(responseBody => showMovies(responseBody.results));
 }
-
 function showMovies(movies) {
+  const result = document.getElementsByClassName("result")[0];
+  result.classList.remove("hidden");
+  result.innerHTML = "";
   for (let i = 0; i < movies.length; i++) {
-    showMovie(movies[i]);
+    let movieElement = getMovieElement(movies[i]);
+    result.append(movieElement);
   }
 }
-
-function showMovie(movie) {
-  console.log(movie.title);
+function getMovieElement(movie) {
+  const movieElement = document.createElement("div");
+  movieElement.className = "movie";
+  movieElement.innerHTML = `
+        <img class="movie-poster" src="https://image.tmdb.org/t/p/w185${movie.poster_path}">
+        <div class="movie-content">
+          <div class="movie-title">${movie.title}</div>
+          <div class="movie-overview">
+            ${movie.overview}
+          </div>
+          </div>
+   `;
+  return movieElement;
 }
